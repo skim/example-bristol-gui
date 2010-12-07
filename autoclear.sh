6bfdb6e6db429ad9d3cc717ac79546a655cd7567
Martin Voigt <voigt.m@gmail.com>
2010-12-07 16:54:29 +0100

    autoclear added to remove the autotools mess

---
+++ autoclear.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm *.m4
+rm -rf *.cache
+rm bristolgui
+rm compile
+rm config.*
+rm configure
+rm depcomp
+rm install-sh
+rm Makefile
+rm Makefile.in
+rm missing
+rm INSTALL
+rm Source/all/*.o
+
+
