3886c1f9d8737d3b09ed2085267f433b321ef1c1
Martin Voigt <voigt.m@gmail.com>
2010-12-21 13:16:15 +0100

    formatted headers

--- Source/all/include/l_option.h
+++ Source/all/include/l_option.h
@@ -28,17 +28,17 @@ typedef struct {
 	GHashTable *map_values;
 } LOptionList;
 
-LOption*		l_option_new_string(const char *id, const char *flag);
-LOption*		l_option_new_int(const char *id, const char *flag);
-LOptionList*	l_option_list_new();
-void			l_option_list_put_option(LOptionList *list, LOption *option);
-LOption*		l_option_list_get_option(LOptionList *list, const char *id);
-LOption* 		l_option_list_nth_option(LOptionList *list, int index);
-void			l_option_list_set_value_string(LOptionList *list, const char *id, const char *value);
-void			l_option_list_set_value_int(LOptionList *list, const char *id, int value);
-LOptionValue*	l_option_list_get_value(LOptionList *list, const char *id);
-int				l_option_list_length_options(LOptionList *list);
-int 			l_option_list_length_values(LOptionList *list);
-char*			l_option_list_render_cli(LOptionList *list);
+LOption*		l_option_new_string				(const char *id, const char *flag);
+LOption*		l_option_new_int				(const char *id, const char *flag);
+LOptionList*	l_option_list_new				();
+void			l_option_list_put_option		(LOptionList *list, LOption *option);
+LOption*		l_option_list_get_option		(LOptionList *list, const char *id);
+LOption* 		l_option_list_nth_option		(LOptionList *list, int index);
+void			l_option_list_set_value_string	(LOptionList *list, const char *id, const char *value);
+void			l_option_list_set_value_int		(LOptionList *list, const char *id, int value);
+LOptionValue*	l_option_list_get_value			(LOptionList *list, const char *id);
+int				l_option_list_length_options	(LOptionList *list);
+int 			l_option_list_length_values		(LOptionList *list);
+char*			l_option_list_render_cli		(LOptionList *list);
 
 #endif /* L_OPTION_H_ */
