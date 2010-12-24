c16e6f62075a2dc772d2b21041687763e367fb2e
Martin Voigt <voigt.m@gmail.com>
2010-12-24 05:54:03 +0100

    combo box now backed by LValue

--- Source/all/ltk_common.c
+++ Source/all/ltk_common.c
@@ -11,21 +11,17 @@ void ltk_combo_box_fill(GtkComboBox *combo_box, LValueList *values) {
 	g_assert(combo_box != NULL);
 	g_assert(values != NULL);
 	GtkListStore *store = NULL;
-	if (l_value_list_get_type(values) == L_TYPE_STRING) {
-		store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
-		GtkTreeIter iter;
-		int i;
-		for (i = 0; i < l_value_list_length(values); i++) {
-			gtk_list_store_append(store, &iter);
-			LValue *value = l_value_list_nth_value(values, i);
-			gtk_list_store_set(store, &iter, 0, l_value_list_nth_display_name(values, i), 1, l_value_get_string(value), -1);
-		}
-		gtk_combo_box_set_model(combo_box, GTK_TREE_MODEL(store));
-		GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
-		gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combo_box), renderer, FALSE);
-		gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combo_box), renderer, "text", 0, NULL);
-		gtk_combo_box_set_active(combo_box, 0);
-	} else {
-		g_warning("type of value list is currently unsupported");
+	store = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_POINTER);
+	GtkTreeIter iter;
+	int i;
+	for (i = 0; i < l_value_list_length(values); i++) {
+		gtk_list_store_append(store, &iter);
+		LValue *value = l_value_list_nth_value(values, i);
+		gtk_list_store_set(store, &iter, 0, l_value_list_nth_display_name(values, i), 1, value, -1);
 	}
+	gtk_combo_box_set_model(combo_box, GTK_TREE_MODEL(store));
+	GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
+	gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(combo_box), renderer, FALSE);
+	gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(combo_box), renderer, "text", 0, NULL);
+	gtk_combo_box_set_active(combo_box, 0);
 }
