5dabfaa6df58edcc8cede34d22557c7ddd7d4ea1
Martin Voigt <voigt.m@gmail.com>
2010-12-27 05:27:48 +0100

    extracted ltk_combo_box.h from ltk_common.h

--- Source/all/include/ltk.h
+++ Source/all/include/ltk.h
@@ -2,6 +2,7 @@
 #define LTK_H_
 
 #include "ltk_common.h"
+#include "ltk_combo_box.h"
 #include "ltk_builder.h"
 #include "ltk_switch.h"
 
--- Source/all/include/ltk_common.h
+++ Source/all/include/ltk_common.h
@@ -6,12 +6,15 @@
 
 #define	LTK_OBJECT_MARK_KEY	"LTK_OBJECT"
 
+typedef struct {
+	LOptionList *options;
+	char *id;
+} LtkPayload ;
+
+LtkPayload* ltk_payload_new								(LOptionList *options, const char *id);
+void 		ltk_object_mark								(GtkObject *object);
+gboolean 	ltk_object_is_marked						(GtkObject *object);
 void 		ltk_button_set_stock_image					(GtkButton *button, const char *stock_name, GtkIconSize size);
-void		ltk_combo_box_fill							(GtkComboBox *combo_box, LValueList *values);
-void		ltk_combo_box_set_active_value				(GtkComboBox *combo_box, LValue *value);
-LValue*		ltk_combo_box_get_active_value				(GtkComboBox *combo_box);
-void		ltk_combo_box_set_active_value_from_option	(GtkComboBox *combo_box, LOptionList *options, const char *id);
-void 		ltk_combo_box_connect_option				(GtkComboBox *combo_box, LOptionList *options, const char *id);
 void		ltk_toggle_button_set_active_from_option	(GtkToggleButton *toggle, LOptionList *options, const char *id);
 void		ltk_toggle_button_connect_option			(GtkToggleButton *toggle, LOptionList *options, const char *id);
 
