527d33afdcb1a8f06353d69ac2926ad1e52005e5
Martin Voigt <voigt.m@gmail.com>
2011-01-24 14:17:24 +0100

    fixed small bug

--- Source/all/profile.c
+++ Source/all/profile.c
@@ -12,11 +12,11 @@ LProfile* bg_profile_new() {
 	l_profile_add_named_option_choice_string(profile, "engine", "oss", "OSS");
 
 	l_profile_set_option_value_int(profile, "samplerate", 48000);
-	l_profile_add_named_option_choice_int(profile, "samplerate", 11025, "11,025 Hz - low MPEG");
-	l_profile_add_named_option_choice_int(profile, "samplerate", 22050, "22,025 Hz - mid MPEG");
-	l_profile_add_named_option_choice_int(profile, "samplerate", 44100, "44.100 Hz - CD, MPEG-1");
-	l_profile_add_named_option_choice_int(profile, "samplerate", 48000, "48,000 Hz - HiFi Standard");
-	l_profile_add_named_option_choice_int(profile, "samplerate", 96000, "96,000 Hz - DVD-Audio");
+	l_profile_add_named_option_choice_int(profile, "samplerate", 11025, "11,025 - low MPEG");
+	l_profile_add_named_option_choice_int(profile, "samplerate", 22050, "22,025 - mid MPEG");
+	l_profile_add_named_option_choice_int(profile, "samplerate", 44100, "44.100 - CD-Audio, MPEG-1");
+	l_profile_add_named_option_choice_int(profile, "samplerate", 48000, "48,000 - HiFi Standard");
+	l_profile_add_named_option_choice_int(profile, "samplerate", 96000, "96,000 - DVD-Audio");
 
 	return profile;
 }
@@ -26,11 +26,21 @@ void bg_profile_connect(LProfile *profile, GtkBuilder *builder) {
 	g_assert(builder != NULL);
 
 	GtkComboBox *combo_engine = ltk_builder_get_combo_box(builder, "combo_engine");
+	GList *choices_engine = l_profile_get_option_choices(profile, "engine");
+	GList *names_engine = l_profile_get_option_choices_names(profile, "engine");
+	char *value_engine = l_profile_get_option_value_string(profile, "engine");
 	ltk_combo_box_set_cell_renderer_text(combo_engine);
-	ltk_combo_box_set_choices_string(combo_engine, l_profile_get_option_choices_names(profile, "engine"));
+	ltk_combo_box_set_choices_string(combo_engine, names_engine);
+	gtk_combo_box_set_active(combo_engine, l_list_index(choices_engine, value_engine, l_string_compare));
 
 	GtkComboBox *combo_samplerate = ltk_builder_get_combo_box(builder, "combo_samplerate");
+	GList *choices_samplerate = l_profile_get_option_choices(profile, "samplerate");
+	GList *names_samplerate = l_profile_get_option_choices_names(profile, "samplerate");
+	LWrapInt *value_samplerate = l_wrap_int(l_profile_get_option_value_int(profile, "samplerate"));
+	g_debug("%d", value_samplerate->value);
 	ltk_combo_box_set_cell_renderer_text(combo_samplerate);
-	ltk_combo_box_set_choices_string(combo_samplerate, l_profile_get_option_choices_names(profile, "samplerate"));
+	ltk_combo_box_set_choices_string(combo_samplerate, names_samplerate);
+	gtk_combo_box_set_active(combo_samplerate, l_list_index(choices_samplerate, value_samplerate, l_wrap_int_compare));
+
 
 }
--- Source/all/session.c
+++ Source/all/session.c
@@ -13,6 +13,10 @@ LSession* bg_session_connect(LSession *session, GtkBuilder *builder) {
 	g_assert(builder != NULL);
 	LProfile *profile = l_session_get_selected_profile(session);
 	g_assert(profile != NULL);
-	ltk_combo_box_set_choices_string(ltk_builder_get_combo_box(builder, "combo_profile"), l_session_get_profile_names(session));
+	GtkComboBox *combo = ltk_builder_get_combo_box(builder, "combo_profile");
+	ltk_combo_box_set_cell_renderer_text(combo);
+	GList *names = l_session_get_profile_names(session);
+	ltk_combo_box_set_choices_string(combo, names);
+	gtk_combo_box_set_active(combo, l_list_index(names, l_session_get_selected_profile_name(session), l_string_compare));
 	bg_profile_connect(profile, builder);
 }
